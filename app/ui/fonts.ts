4abfbbd2359b3a0bd34fab0bd05c0376e0763467
gerritvreeman <49799950+gerritvreeman@users.noreply.github.com>
2024-10-14 14:19:44 -0500

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana, Ubuntu, Open_Sans } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ["400", "700"] });
+export const ubuntu = Ubuntu({ subsets: ['latin'], weight: ["300", "400", "500", "700"] });
+export const openSans = Open_Sans({ subsets: ['latin'] });
